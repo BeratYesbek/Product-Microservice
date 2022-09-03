330bffa9a3510e8bc46a2f4dd556dd51139cf76b
Feanor <beratyesbek@gmail.com>
2022-09-03 09:14:17 +0300

    Some service and manager class created

---
+++ Business/Abstracts/IServiceRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities.Abstracts;
+
+namespace Business.Abstracts
+{
+    public interface IServiceRepository<T>
+    {
+        IDataResult<T> Create(T entity);
+        IResult Update(T entity);
+        IResult Delete(int id);
+        IDataResult<T> Get(int id);
+        IDataResult<List<T>> GetAll();
+    }
+}
