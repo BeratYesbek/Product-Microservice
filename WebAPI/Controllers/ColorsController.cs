2e95cf40a17c59ad7a9fdeedda4160eb72a29e01
Feanor <beratyesbek@gmail.com>
2022-09-04 19:59:23 +0300

    Color controller and Category controller have been created

---
+++ WebAPI/Controllers/ColorsController.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstracts;
+using Entities.Modals;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColorsController : ControllerBase
+    {
+       /* private readonly IColorService _colorService;
+        public ColorsController(IColorService colorService)
+        {
+            _colorService = colorService;
+        }*/
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
+        public IActionResult Create(Color color)
+        {
+            return Created("", "Merhaba");
+
+            /* var result = _colorService.Create(color);
+             if (result.Success)
+             {
+             }
+             return BadRequest(result);*/
+        }
+    }
+}
